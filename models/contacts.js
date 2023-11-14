256181f1d52ca9d63c8b8576bc9acb45ece9f470
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-11-14 18:59:43 +0100

    end hw 2

--- models/contacts.js
+++ models/contacts.js
@@ -1,27 +1,63 @@
 const fs = require("fs/promises");
 const path = require("path");
+const {nanoid} = require("nanoid");
+const { constants } = require("buffer");
+
 
 const contactsFilePath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  try {
-    const jsonData = await fs.readFile(contactsFilePath, "utf-8");
-    const data = JSON.parse(jsonData);
-   return data
-    
-  } catch (error) {
-    console.error("Ошибка при чтении файла контактов:", error.message);
-  }
- 
+  const jsonData = await fs.readFile(contactsFilePath, "utf-8");
+  const data = JSON.parse(jsonData);
+  return data;
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((c) => c.id === contactId);
 
-const removeContact = async (contactId) => {};
+  return contact || null;
+};
 
-const addContact = async (body) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item=> item.id === contactId)
+  if (index === -1) {
+    return null
+    
+  }
+  const [result] = contacts.splice(index, 1)
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+  return result
 
-const updateContact = async (contactId, body) => {};
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item=> item.id === contactId)
+  if (index === -1) {
+    return null
+    
+  }
+  contacts[index] = { ...contacts[index], ...body }
+  console.log(body)
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+  
+};
 
 module.exports = {
   listContacts,
