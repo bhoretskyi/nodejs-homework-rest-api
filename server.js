3f829cbafc982fce55b1053056fb061d3173f854
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-11-19 18:38:57 +0100

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Bohdan:wBImOG9XQAyzVKz9@cluster0.ambzzpa.mongodb.net/db-contacts?retryWrites=true&w=majority";
-// wBImOG9XQAyzVKz9
+// const{DB_HOST} = require('./config')
+const {DB_HOST} = process.env
+// console.log(process.env)
 mongoose
   .connect(DB_HOST)
   .then(() => {
